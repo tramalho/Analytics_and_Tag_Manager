ab6f53d2634107cbb8ca8e0bdfe82e2d6eab37f3
trama <thiagofacul@gmail.com>
2016-08-08 12:40:04 -0300

    [Introducao a analytics] Write Code to Create Tracker

--- DinnerApp_BEGIN/app/src/main/java/com/example/android/dinnerapp/MyApplication.java
+++ DinnerApp_BEGIN/app/src/main/java/com/example/android/dinnerapp/MyApplication.java
@@ -2,8 +2,27 @@ package com.example.android.dinnerapp;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Created by trama on 08/08/16.
  */
 public class MyApplication extends Application{
+
+    private Tracker mTracker;
+
+    public void startTracking(){
+
+        if(mTracker == null){
+            GoogleAnalytics instance = GoogleAnalytics.getInstance(this);
+            mTracker = instance.newTracker(R.xml.track_app);
+            mTracker.enableAutoActivityTracking(true);
+        }
+    }
+
+    public Tracker getTracker() {
+        startTracking();
+        return mTracker;
+    }
 }
